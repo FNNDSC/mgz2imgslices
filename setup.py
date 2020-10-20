cc52c1c6655d0b35c6d9f0896a82733490886a50
Arushi Vyas <pittsburgh@Arushis-Macbook-Pro.local>
2020-10-20 16:11:02 -0400

    bug fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'mgz2imgslices',
-      version          =   '1.4.40',
+      version          =   '1.4.42',
       description      =   '(Python) utility to filter mgz volumes to per-voxel-value directories of jpg/png image slices',
       long_description =   readme(),
       author           =   'FNNDSC',
